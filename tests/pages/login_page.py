52f7a4848399046647135e71d354f3cac8ace8d3
Saransh Tiwari <saranshtiwari96@gmail.com>
2026-09-18 00:43:31 +0200

    Align LoginPage with HomePage: named user-facing locators

--- tests/pages/login_page.py
+++ tests/pages/login_page.py
@@ -1,11 +1,15 @@
 class LoginPage:
     def __init__(self, page):
         self.page = page
+        self.username_input = page.get_by_role("textbox", name="Username Or Email Address", exact=True)
+        self.password_input = page.get_by_role("textbox", name="Password", exact=True)
+        self.login_button = page.get_by_role("button", name="Login", exact=True) 
 
     def goto(self):
         self.page.goto("http://localhost:3456/login")
 
     def login(self, username, password):
-        self.page.fill("#username", username)
-        self.page.fill("#password", password)
-        self.page.get_by_role("button", name="Login").click()
+        self.username_input.fill(username)
+        self.password_input.fill(password)
+        self.login_button.click()
+        
